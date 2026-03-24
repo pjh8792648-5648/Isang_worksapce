32964e30a328231237bbb9d9eb2b8e19ca7139a3
pjh8792648-5648 <pjh8792648@gmail.com>
2026-03-24 13:41:50 +0900

    supabase.js 업데이트

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dlxzqartjnevsihdqyrf.supabase.co'
+const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRseHpxYXJ0am5ldnNpaGRxeXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMjEyNzIsImV4cCI6MjA4OTg5NzI3Mn0.Xol5jvsyyiL2OUki6QhCffxVDZLaOxYR_jRijgZ1Kfg'
 const supabaseKey = 'sb_publishable_vpoRKTgn2ZsaZ0Fx19yMiA_pGnipxYd'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
